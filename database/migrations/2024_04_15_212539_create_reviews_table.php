684649bdde44f1aa9d16b2326fb84a06722cfa84
Lawrence Emeka <mrabarah@gmail.com>
2024-04-16 01:19:17 +0100

    laravel 10 book review

---
+++ database/migrations/2024_04_15_212539_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+
+            // this column will reference the other table
+            // $table->unsignedBigInteger('book_id');
+
+            $table->text('review');
+            $table->unsignedTinyInteger('rating'); 
+            $table->timestamps();
+
+            // to define a foreign key we say
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
+            // a simple relationship that will replaces line 18 and 24
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
